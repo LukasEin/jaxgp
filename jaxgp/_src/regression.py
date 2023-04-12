f0a011fbe833c66ead9cbbc5aadba3ec138c8357
Lukas Einramhof <lukas.einramhof@gmail.com>
2023-04-12 10:49:33 +0200

    split kernelparams and noise up

--- jaxgp/_src/regression.py
+++ jaxgp/_src/regression.py
@@ -24,9 +24,8 @@ class BaseGPR:
 
         # defines parameters of the regressor and sets flag for optimizing the noise parameter as well
         self.optimize_noise = optimize_noise
-        self.kernel_params = init_kernel_params
+        self.kernel_params = jnp.array(init_kernel_params)
         self.noise = noise
-        self.params = jnp.array((noise,) + init_kernel_params)
 
         # initialized variables to save from the fitting step
         self.fit_vector = None
@@ -47,9 +46,9 @@ class BaseGPR:
         self.further_args.append(jnp.split(X_data, sum_splits))
 
         self.optimize(Y_data, *self.further_args)
-        self.fit_matrix, self.fit_vector = self.forward(self.params, Y_data, *self.further_args)
+        self.fit_matrix, self.fit_vector = self.forward(self.kernel_params, self.noise, Y_data, *self.further_args)
 
-    def forward(self, params: Array, Y_data: Array, *further_args) -> Tuple[Array, Array]:
+    def forward(self, kernel_params: Array, noise, Y_data: Array, *further_args) -> Tuple[Array, Array]:
         raise NotImplementedError("Forward method not yet implemented!")
     
     def eval(self, X: Array, return_std=False) -> Union[Tuple[Array, Array], Array]:
@@ -64,9 +63,9 @@ class BaseGPR:
             raise ValueError("GPR not correctly fitted!")
         
         if return_std:
-            return self._meanstd(self.params, X, self.fit_matrix, self.fit_vector, *self.further_args)
+            return self._meanstd(self.kernel_params, self.noise, X, self.fit_matrix, self.fit_vector, *self.further_args)
         
-        return self._mean(self.params, X, self.fit_matrix, self.fit_vector, *self.further_args)
+        return self._mean(self.kernel_params, self.noise, X, self.fit_matrix, self.fit_vector, *self.further_args)
     
     def _mean(self, params: Array, X: Array, fitmatrix: Array, fitvector: Array, *further_args) -> Array: raise NotImplementedError
     def _meanstd(self, params: Array, X: Array, fitmatrix: Array, fitvector: Array, *further_args) -> Tuple[Array, Array]: raise NotImplementedError
@@ -89,7 +88,7 @@ class BaseGPR:
             result = solver.run(self.kernel_params, (1e-3,jnp.inf), self.noise, Y_data, *further_args)
 
 
-            self.params = jnp.array((self.noise, ) + result.params)
+            self.kernel_params = result.params
     
     @partial(vmap, in_axes=(None, None, 0))
     def _build_xTAx(self, A: Array, X: Array) -> Array:
@@ -111,7 +110,7 @@ class BaseGPR:
 
             Builds a vector of the covariance of all X[:,i] with them selves.
         '''
-        func = lambda A: self.kernel.eval_func(A, A, params)
+        func = lambda A: self.kernel.eval(A, A, params)
         func = vmap(func, in_axes=(0))
         return func(X)
 
@@ -125,7 +124,7 @@ class BaseGPR:
             Builds the covariance matrix between the elements of X1 and X2
             based on inputs representing values of the target function.
         '''
-        func = lambda A, B: self.kernel.eval_func(A, B, params)
+        func = lambda A, B: self.kernel.eval(A, B, params)
         func = vmap(vmap(func, in_axes=(None,0)), in_axes=(0,None))
         return func(X1, X2)
     
@@ -143,7 +142,7 @@ class BaseGPR:
             based on X1 representing values of the target function and X2
             representing derivative values of the target function.
         '''
-        func = lambda A, B: self.kernel.eval_dx2(A,B, index, params) 
+        func = lambda A, B: self.kernel.grad2(A,B, index, params) 
         func = vmap(vmap(func, in_axes=(None,0)), in_axes=(0,None))
         return func(X1, X2)
     
@@ -161,46 +160,46 @@ class BaseGPR:
             based on X1 and X2 representing derivative values of the target 
             function.
         '''
-        func = lambda A, B: self.kernel.eval_ddx1x2(A, B, index_1, index_2, params) 
+        func = lambda A, B: self.kernel.jac(A, B, index_1, index_2, params) 
         func = vmap(vmap(func, in_axes=(None,0)), in_axes=(0,None))
         return func(X1, X2)
 
 class ExactGPR(BaseGPR):
-    def __init__(self, kernel=RBF(), data_split=(0, 0), kernel_params=(1.0,), noise=1e-4, *, optimize_noise=False, noise_prior=None, kernel_prior=None) -> None:
-        super().__init__(kernel, data_split, kernel_params, noise, noise_prior=noise_prior, kernel_prior=kernel_prior, optimize_noise=optimize_noise)
+    def __init__(self, kernel=RBF(), data_split=(0, 0), kernel_params=(1.0,), noise=1e-4, *, optimize_noise=False) -> None:
+        super().__init__(kernel, data_split, kernel_params, noise, optimize_noise=optimize_noise)
 
         self.further_args = []
 
     @partial(jit, static_argnums=(0))
-    def forward(self, params: Array, Y_data: Array, X_split: list[Array]) -> Tuple[Array, Array]:
+    def forward(self, kernel_params: Array, noise, Y_data: Array, X_split: list[Array]) -> Tuple[Array, Array]:
         # Build the full covariance Matrix between all datapoints in X_data depending on if they   
         # represent function evaluations or derivative evaluations
-        K_NN = self._CovMatrix_Kernel(X_split[0], X_split[0], params=params[1:])
+        K_NN = self._CovMatrix_Kernel(X_split[0], X_split[0], params=kernel_params)
         for i,elem in enumerate(X_split[1:]):
-            K_mix = self._CovMatrix_Grad(X_split[0], elem, index=i, params=params[1:])
+            K_mix = self._CovMatrix_Grad(X_split[0], elem, index=i, params=kernel_params)
             K_NN = jnp.concatenate((K_NN,K_mix),axis=1)
         
         for i,elem_1 in enumerate(X_split[1:]):
-            K_mix = self._CovMatrix_Grad(X_split[0], elem_1, index=i, params=params[1:])
+            K_mix = self._CovMatrix_Grad(X_split[0], elem_1, index=i, params=kernel_params)
 
             for j,elem_2 in enumerate(X_split[1:]):
-                K_derivs = self._CovMatrix_Hess(elem_1, elem_2, index_1=i, index_2=j, params=params[1:])
+                K_derivs = self._CovMatrix_Hess(elem_1, elem_2, index_1=i, index_2=j, params=kernel_params)
                 K_mix = jnp.concatenate((K_mix,K_derivs),axis=0)
 
             K_NN = jnp.concatenate((K_NN,K_mix.T),axis=0)
 
         # additional small diagonal element added for 
         # numerical stability of the inversion and determinant
-        return (jnp.eye(len(K_NN)) * (params[0]**2 + 1e-6) + K_NN), (Y_data)
+        return (jnp.eye(len(K_NN)) * (noise**2 + 1e-6) + K_NN), (Y_data)
     
     @partial(jit, static_argnums=(0,))
-    def _mean(self, params: Array, X: Array, fitmatrix: Array, fitvector: Array, X_split: Array) -> Array:
+    def _mean(self, kernel_params: Array, noise, X: Array, fitmatrix: Array, fitvector: Array, X_split: Array) -> Array:
         '''
             calculates the posterior mean for all points in X
         '''
-        full_vectors = self._CovMatrix_Kernel(X, X_split[0], params=params[1:])
+        full_vectors = self._CovMatrix_Kernel(X, X_split[0], params=kernel_params)
         for i,elem in enumerate(X_split[1:]):
-            deriv_vectors = self._CovMatrix_Grad(X, elem, index=i, params=params[1:])
+            deriv_vectors = self._CovMatrix_Grad(X, elem, index=i, params=kernel_params)
             full_vectors = jnp.concatenate((full_vectors,deriv_vectors),axis=1)
 
         means = full_vectors@solve(fitmatrix,fitvector,assume_a="pos")
@@ -208,18 +207,18 @@ class ExactGPR(BaseGPR):
         return means
     
     @partial(jit, static_argnums=(0,))
-    def _meanstd(self, params: Array, X: Array, fitmatrix: Array, fitvector: Array, X_split: Array) -> Tuple[Array, Array]:
+    def _meanstd(self, kernel_params: Array, noise, X: Array, fitmatrix: Array, fitvector: Array, X_split: Array) -> Tuple[Array, Array]:
         '''
             calculates the posterior mean and std for all points in X
         '''
-        full_vectors = self._CovMatrix_Kernel(X, X_split[0], params=params[1:])
+        full_vectors = self._CovMatrix_Kernel(X, X_split[0], params=kernel_params)
         for i,elem in enumerate(X_split[1:]):
-            deriv_vectors = self._CovMatrix_Grad(X, elem, index=i, params=params[1:])
+            deriv_vectors = self._CovMatrix_Grad(X, elem, index=i, params=kernel_params)
             full_vectors = jnp.concatenate((full_vectors,deriv_vectors),axis=1)
 
         means = full_vectors@solve(fitmatrix,fitvector,assume_a="pos")
 
-        X_cov = self._CovVector_Id(X, params[1:])  
+        X_cov = self._CovVector_Id(X, kernel_params)  
         temp = self._build_xTAx(fitmatrix, full_vectors)      
         stds = jnp.sqrt(X_cov - temp) # no noise term in the variance
         
@@ -271,9 +270,8 @@ class ExactGPR(BaseGPR):
             result is multiplied with a small coefficient for numerical stability 
                 of the optimizer
         '''
-        params = jnp.array((noise,) + kernel_params)
         # calculates the full covariance matrix
-        fit_matrix, fit_vector = self.forward(params, Y_data, X_split)
+        fit_matrix, fit_vector = self.forward(kernel_params, noise, Y_data, X_split)
         fit_vector = fit_vector.reshape(-1)
 
         # calculates the logdet of the full covariance matrix
@@ -285,57 +283,57 @@ class ExactGPR(BaseGPR):
         return mle / 20000.0
 
 class SparseGPR(BaseGPR):
-    def __init__(self, kernel=RBF(), data_split=(0,), X_ref=None, kernel_params=(1.0,), noise= 1e-4, *, optimize_noise=False, noise_prior=None, kernel_prior=None) -> None:
+    def __init__(self, kernel=RBF(), data_split=(0,), X_ref=None, kernel_params=(1.0,), noise= 1e-4, *, optimize_noise=False) -> None:
         '''
             Sparsifies the full GP regressor by the Projected Process Approximation.
         '''
-        super().__init__(kernel, data_split, kernel_params, noise, optimize_noise=optimize_noise, noise_prior=noise_prior, kernel_prior=kernel_prior)
+        super().__init__(kernel, data_split, kernel_params, noise, optimize_noise=optimize_noise)
 
         self.further_args = [X_ref, ]
 
     @partial(jit, static_argnums=(0,))
-    def forward(self, params: Array, Y_data: Array, X_ref: Array, X_split: list[Array]) -> Tuple[Array, Array]:
+    def forward(self, kernel_params: Array, noise, Y_data: Array, X_ref: Array, X_split: list[Array]) -> Tuple[Array, Array]:
         # calculates the covariance between the training points and the reference points
-        K_MN = self._CovMatrix_Kernel(X_ref, X_split[0], params[1:])
+        K_MN = self._CovMatrix_Kernel(X_ref, X_split[0], kernel_params)
         for i,elem in enumerate(X_split[1:]):
-            K_deriv = self._CovMatrix_Grad(X_ref, elem, index=i, params=params[1:])
+            K_deriv = self._CovMatrix_Grad(X_ref, elem, index=i, params=kernel_params)
             K_MN = jnp.concatenate((K_MN,K_deriv),axis=1)
 
         # calculates the covariance between each pair of reference points
-        K_ref = self._CovMatrix_Kernel(X_ref, X_ref, params=params[1:])
+        K_ref = self._CovMatrix_Kernel(X_ref, X_ref, params=kernel_params)
             
         # added small positive diagonal to make the matrix positive definite
-        fit_matrix = params[0]**2 * K_ref + K_MN@K_MN.T + jnp.eye(len(X_ref)) * 1e-4
+        fit_matrix = noise**2 * K_ref + K_MN@K_MN.T + jnp.eye(len(X_ref)) * 1e-4
         fit_vector = K_MN@Y_data
         return fit_matrix, fit_vector
     
     @partial(jit, static_argnums=(0,))
-    def _mean(self, params: Array, X, fitmatrix: Array, fitvector: Array, X_ref: Array, X_split: list[Array]) -> Array:
+    def _mean(self, kernel_params: Array, noise, X, fitmatrix: Array, fitvector: Array, X_ref: Array, X_split: list[Array]) -> Array:
         '''
             calculates the posterior mean for all points in X
         '''
         # calculates the caovariance between the test points and the reference points
-        ref_vectors = self._CovMatrix_Kernel(X, X_ref, params[1:])
+        ref_vectors = self._CovMatrix_Kernel(X, X_ref, kernel_params)
         
         means = ref_vectors@solve(fitmatrix,fitvector)#,assume_a="pos")
         return means
     
     @partial(jit, static_argnums=(0,))
-    def _meanstd(self, params: Array, X: Array, fitmatrix: Array, fitvector: Array, X_ref: Array, X_split: list[Array]) -> Tuple[Array, Array]:
+    def _meanstd(self, kernel_params: Array, noise, X: Array, fitmatrix: Array, fitvector: Array, X_ref: Array, X_split: list[Array]) -> Tuple[Array, Array]:
         '''
             calculates the posterior mean and std for all points in X
         '''
         # calculates the caovariance between the test points and the reference points
-        ref_vectors = self._CovMatrix_Kernel(X, X_ref, params[1:])
+        ref_vectors = self._CovMatrix_Kernel(X, X_ref, kernel_params)
 
         means = ref_vectors@solve(fitmatrix,fitvector)#,assume_a="pos")
 
-        X_cov = self._CovVector_Id(X, params[1:])
+        X_cov = self._CovVector_Id(X, kernel_params)
 
-        K_ref = self._CovMatrix_Kernel(X_ref, X_ref, params=params[1:])
+        K_ref = self._CovMatrix_Kernel(X_ref, X_ref, params=kernel_params)
 
         first_temp = self._build_xTAx(K_ref + jnp.eye(len(X_ref)) * 1e-4, ref_vectors)
-        second_temp = params[0]**2 * self._build_xTAx(fitmatrix, ref_vectors)
+        second_temp = noise**2 * self._build_xTAx(fitmatrix, ref_vectors)
         
         stds = jnp.sqrt(X_cov - first_temp + second_temp) # no noise term in the variance
         
