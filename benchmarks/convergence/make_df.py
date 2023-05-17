5bdcd51d12181c450eb84749a4ddea84dbd168cf
Lukas Einramhof <lukas.einramhof@gmail.com>
2023-05-17 13:50:19 +0200

    updated benchmark stuff

--- benchmarks/convergence/make_df.py
+++ benchmarks/convergence/make_df.py
@@ -39,6 +39,29 @@ def make_dict(num_f_vals, num_d_vals, optimizer, fun, eval_grid, predictions, st
     
     return {**temp, **mses, **confs}
 
+def make_dict_all(num_vals, optimizer, fun, eval_grid, predictions, stds):
+    Y = fun(eval_grid)
+
+    true_in_confs = []
+    for std, pred in zip(stds, predictions):
+        true_in_confs.append(true_in_conf(Y, pred, std))
+
+    temp = {"f": num_vals, "opt": optimizer, 
+            "mses": vmap(mse, in_axes=(None, 0))(Y, predictions),
+            "maxerrs": vmap(maxerr, in_axes=(None, 0))(Y, predictions),
+            "maxstds": vmap(maxstd, in_axes=(0,))(stds),
+            "true_in_confs": jnp.array(true_in_confs)}
+    
+    mses = {"mean_mse": jnp.mean(temp["mses"]), 
+         "max_mse": jnp.max(temp["mses"]),
+         "min_mse": jnp.min(temp["mses"])}
+    
+    confs = {"mean_tic": jnp.mean(temp["true_in_confs"]), 
+         "max_tic": jnp.max(temp["true_in_confs"]),
+         "min_tic": jnp.min(temp["true_in_confs"])}
+    
+    return {**temp, **mses, **confs}
+
 def make_df(list_f_vals, list_d_vals, optimizers, in_dir, name, sparse, subset_size, fun, eval_grid):
     in_list = []
 
@@ -71,5 +94,39 @@ def make_df(list_f_vals, list_d_vals, optimizers, in_dir, name, sparse, subset_s
                 stds = jnp.array(stds_list)
 
                 in_list.append(make_dict(num_f_vals, num_d_vals, optimizer, fun, eval_grid, means, stds))
+
+    return pd.DataFrame(in_list)
+
+def make_df_all(list_vals, optimizers, in_dir, name, sparse, subset_size, fun, eval_grid):
+    in_list = []
+
+    for num_vals in list_vals:        
+        for optimizer in optimizers:
+            if sparse:
+                fname = f"{in_dir}/{name}_v{num_vals}_{optimizer}_sparse{subset_size}"
+
+                means = jnp.load(f"{fname}_means.npz")
+                stds = jnp.load(f"{fname}_stds.npz")
+            else:
+                fname = f"{in_dir}/{name}_v{num_vals}"
+
+                means = jnp.load(f"{fname}means{optimizer}.npz")
+                stds = jnp.load(f"{fname}stds{optimizer}.npz")
+            
+            means_list = []
+            for key, value in means.items():
+                means_list.append(value)
+            stds_list = []
+            for key, value in stds.items():
+                stds_list.append(value)
+
+            # for i, (mean, std) in enumerate(zip(means, stds)):
+            #     is_nan = jnp.any(jnp.isnan(mean)) or jnp.any(jnp.isnan(mean))
+            #     if is_nan:
+            #         print(f"Nan values at f={num_f_vals}, d={num_d_vals}, opt={optimizer}, iter={i}!")
+            means = jnp.array(means_list)
+            stds = jnp.array(stds_list)
+
+            in_list.append(make_dict_all(num_vals, optimizer, fun, eval_grid, means, stds))
 
     return pd.DataFrame(in_list)
