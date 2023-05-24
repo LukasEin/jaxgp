301dd3b5d7fd6125e580be2283232f80ad3d78ca
Lukas Einramhof <lukas.einramhof@gmail.com>
2023-05-24 16:09:27 +0200

    I HAVE NO CLUE WHATS WRONG

--- jaxgp/covar.py
+++ jaxgp/covar.py
@@ -1,3 +1,5 @@
 from ._src.covar import (full_covariance_matrix, full_covariance_matrix_nograd,
                          sparse_covariance_matrix,
                          sparse_covariance_matrix_nograd)
+
+from ._src.covar_module import SparseCovarModule
