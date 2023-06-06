e933fef72f23fd872776871d214705aa5f7a360d
Lukas Einramhof <lukas.einramhof@gmail.com>
2023-06-06 13:15:43 +0200

    fixed logger

--- jaxgp/_src/logger.py
+++ jaxgp/_src/logger.py
@@ -1,18 +1,10 @@
-import time
 from typing import Tuple
-
-import jax.numpy as jnp
 
 
 class Logger:
     '''A simple logger to write out the convergence process of the optimization
     '''
-    def __init__(self, name=None) -> None:
-        if name is None:
-            self.name = f"{time.time()}.log"
-        else:
-            self.name = f"{name}.log"
-
+    def __init__(self) -> None:
         self.buffer = []
         self.iters_list = []
 
@@ -29,17 +21,6 @@ class Logger:
     def write(self):
         '''called after the optimization to save the parameters of the current optimization run
         '''
-        self.iters_list.append(jnp.array(self.buffer))
+        self.iters_list.append(self.buffer)
 
         self.buffer = []
-
-    def log(self, msg: str):
-        '''log sum message
-
-        Parameters
-        ----------
-        msg : str
-            message to append to logfile
-        '''
-        with open(self.name, mode="a") as f:
-            f.write(msg+"\n")
