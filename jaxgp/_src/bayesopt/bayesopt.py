e81722e21ed847f085fa6586583c46d2a95efe4a
Lukas Einramhof <lukas.einramhof@gmail.com>
2023-06-07 14:37:00 +0200

    bayesopt works

--- jaxgp/_src/bayesopt/bayesopt.py
+++ jaxgp/_src/bayesopt/bayesopt.py
@@ -1,89 +1,70 @@
 from dataclasses import dataclass
-from typing import Callable, Tuple, Union
+from typing import Callable, Tuple
 
-import jax
+from jax import jit
 import jax.numpy as jnp
 from jax.numpy import ndarray
 
 from ..kernels import BaseKernel
-from ..covar import FullCovar, SparseCovar
-from ..utils import *
+from ..utils import for_loop
+from ..regression.regression import ExactGPR
 
 
-def _step_full_grad(covar_module: FullCovar, acqui_fun, X_split, Y_data, kernel, kernel_params):
+def _step_full_grad(X_split, Y_data, gpr, acqui_fun, eval_fun):
     # Find new best point
-    X_next, Y_next = acqui_fun()
-
-    # update covariance matrix
-    cov_self = CovMatrixDD(X_next, X_next, kernel, kernel_params)
-
-    cov_grad = CovMatrixDD(X_split[1], X_next, kernel, kernel_params)
-    cov_fun = CovMatrixFD(X_split[0], X_next, kernel, kernel_params)
-    cov_other = jnp.vstack((cov_fun, cov_grad))
-
-    cov = jnp.vstack((jnp.hstack((covar_module.k_nn, cov_other)),
-                      jnp.hstack((cov_other.T, cov_self))))
+    X_next = acqui_fun(gpr)
+    Y_next = eval_fun(X_next)
 
     # update data
     X_next = (X_split[0], jnp.vstack((X_split[1], X_next)))
     Y_next = (Y_data[0], jnp.vstack((Y_data[1], Y_next)))
 
-    return FullCovar(cov), X_next, Y_next
+    return X_next, Y_next
 
-def _step_full_fun(covar_module: FullCovar, acqui_fun, X_split, Y_data, kernel, kernel_params):
+def _step_full_fun(X_split, Y_data, gpr, acqui_fun, eval_fun):
     # Find new best point
-    X_next, Y_next = acqui_fun()
-
-    # update covariance matrix
-    cov_self = CovMatrixFF(X_next, X_next, kernel, kernel_params)
-
-    cov_grad = CovMatrixFD(X_next, X_split[1], kernel, kernel_params)
-    cov_fun = CovMatrixFF(X_next, X_split[0], kernel, kernel_params)
-    cov_other = jnp.hstack((cov_fun, cov_grad))
-
-    cov = jnp.vstack((jnp.hstack((cov_self, cov_other)),
-                      jnp.hstack((cov_other.T, covar_module.k_nn))))
+    X_next = acqui_fun(gpr)
+    Y_next = eval_fun(X_next)
 
     # update data
     X_next = (jnp.vstack((X_split[0], X_next)), X_split[1])
     Y_next = (jnp.vstack((Y_data[0], Y_next)), Y_data[1])
 
-    return FullCovar(cov), X_next, Y_next
-
-def _for_loop(lb, ub, init_val, body_fun):
-    val = init_val
-
-    for i in range(lb, ub):
-        val = body_fun(i, val)
-
-    return val
-
-for_loop = jax.jit(_for_loop, static_argnums=(0,1,3))
+    return X_next, Y_next
 
 @dataclass   
-class BayesOpt:
+class ExactBayesOpt:
     X_split: Tuple[ndarray, ndarray]
-    Y_train: ndarray
+    Y_train: Tuple[ndarray, ndarray]
     kernel: BaseKernel
-    init_kernel_params: ndarray
-    noise: Union[float, ndarray]
     acquisition_func: Callable
-    acqu_extra_args: Tuple
-    optimize_method: str = "L-BFGS-B"
+    eval_func: Callable
+    grad: bool = True
+    optimize_method: str = 0
     logger: Callable = None
 
-    # def __post_init__(self):
-    #     self.result = (self.X_split, self.Y_train)
+    def __post_init__(self) -> None:
+        self.gpr = ExactGPR(self.kernel, optimize_method=self.optimize_method)
 
-    def run(self, num_iters: int) -> None:
+    def __call__(self, num_iters: int) -> None:
+        def acqui_fun(gpr):
+            return self.acquisition_func(gpr, self.logger)
+
+        if self.grad:
+            def _body_fun(X, Y, gpr):
+                return _step_full_grad(X, Y, gpr, acqui_fun, self.eval_func)
+        else:
+            def _body_fun(X, Y, gpr):
+                return _step_full_fun(X, Y, gpr, acqui_fun, self.eval_func)
+            
+        body_fun = jit(_body_fun)
+
         X, Y = self.X_split, self.Y_train
 
-        for i in range(num_iters):
-            X, Y = _bayesoptstep(X, Y, self.init_kernel_params, self.kernel, self.noise, self.optimize_method, self.acquisition_func, i, *self.acqu_extra_args)
+        for _ in range(num_iters):
+            self.gpr.reset_params()
+            self.gpr.train(X, jnp.vstack(Y).reshape(-1))
+            X, Y = body_fun(X, Y, self.gpr)
 
         self.X_split, self.Y_train = X, Y
-        # def for_body(i, input):
-        #     return _bayesoptstep(*input, self.init_kernel_params, self.kernel, self.noise, self.optimize_method, self.acquisition_func, *self.acqu_extra_args)
-        
-        # self.result = jax.lax.fori_loop(0, num_iters, for_body, self.result)
         
--- jaxgp/bayesopt.py
+++ jaxgp/bayesopt.py
@@ -1,2 +1,2 @@
-from ._src.bayesopt.bayesopt import BayesOpt
-from ._src.bayesopt.acquisition import maximum_confidence_grad
+from ._src.bayesopt.bayesopt import ExactBayesOpt
+from ._src.bayesopt.acquisition import UpperConfidenceBound, MaximumVariance
